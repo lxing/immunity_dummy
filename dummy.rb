29f7e7ac6f94e8fb25091be1838c8406d2406d6c
Lawrence Xing <lawrence.xing@ooyala.com>
2012-08-28 15:04:19 -0700

    Added Hastur ping

--- dummy.rb
+++ dummy.rb
@@ -3,11 +3,13 @@
 require "json"
 require "rest-client"
 require "sinatra/base"
+require "hastur"
 
 class Dummy < Sinatra::Base
   set :port, 8123
   
   get '/' do
+    Hastur.counter("immunity_dummy")
     "I am a fish"
   end
 
